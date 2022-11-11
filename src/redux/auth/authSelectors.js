09f3dc8b6420272dfdb399e1ace65c909961ea6b
r-strilets <str.komar@gmail.com>
2022-11-11 13:14:08 +0200

    Update authSelectors.js

--- src/redux/auth/authSelectors.js
+++ src/redux/auth/authSelectors.js
@@ -1,5 +1,5 @@
 export const selectToken = state => state.auth.token;
-export const selectName = state => state.auth.user.name;
+export const selectName = state => state.auth.user.username;
 export const selectEmail = state => state.auth.user.email;
 export const selectIsFetchingCurrentUser = state =>
   state.auth.isFetchingCurrentUser;

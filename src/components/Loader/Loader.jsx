c5077c006df879be4bb9a45148f3ac4aa2585efc
Valeria <valeriastetsiuk3546@gmail.com>
2022-11-11 23:16:52 +0200

    add loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,24 +1,28 @@
 import { ThreeCircles } from 'react-loader-spinner';
-
-export const Loader = () => (
-  <ThreeCircles
-    height="100"
-    width="100"
-    color="#24CCA7"
-    display="block"
-    wrapperStyle={{
-      display: 'block',
-      textAlign: 'center',
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-    }}
-    wrapperClass=""
-    visible={true}
-    ariaLabel="three-circles-rotating"
-    outerCircleColor="rotating"
-    innerCircleColor=""
-    middleCircleColor="rotating"
-  />
-);
+import s from '../Loader/Loader.module.css';
+export default function Loader() {
+  return (
+    <div className={s.loader}>
+      <ThreeCircles
+        height="100"
+        width="100"
+        color="#24CCA7"
+        display="block"
+        wrapperStyle={{
+          display: 'block',
+          textAlign: 'center',
+          left: '50%',
+          right: '50%',
+          top: '50%',
+          bottom: '50%',
+        }}
+        wrapperClass=""
+        visible={true}
+        ariaLabel="three-circles-rotating"
+        outerCircleColor="rotating"
+        innerCircleColor=""
+        middleCircleColor="rotating"
+      />
+    </div>
+  );
+}

bfcd885865c685a61028c8e89b1082703b10d516
BigSiCorp <si.kocorp@gmail.com>
2022-11-12 11:29:01 +0200

    HP commit

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,10 +1,12 @@
 import { Dashboard } from 'components/Dashboard/Dashboard';
 import { Outlet } from 'react-router-dom';
 import { Box } from './HomePage.styled';
+import { ButtonAddTransactions } from 'components/ButtonAddTransaction/ButtonAddTransaction';
 
 export const HomePage = () => {
   return (
     <Box>
+      <ButtonAddTransactions />
       <Dashboard />
       <div>Outlet</div>
       <Outlet />

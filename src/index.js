2cc697366b2521a764e69c176d5619f40f6de205
YevheniiBiloshapka <106679993+YevheniiBiloshapka@users.noreply.github.com>
2022-11-12 16:44:40 +0200

    add basename router

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
-        <BrowserRouter>
+        <BrowserRouter basename="/flowers-wallet/">
           <App />
         </BrowserRouter>
       </PersistGate>
@@ -23,4 +23,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   </React.StrictMode>
 );
 
-//  basename = '/flowers-wallet/';
+//  basename = '/flowers-wallet/'

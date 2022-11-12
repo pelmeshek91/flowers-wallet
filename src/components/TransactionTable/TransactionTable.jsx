188354d197c99b7e57c09b378373cdf05ee2fee9
r-strilets <str.komar@gmail.com>
2022-11-12 23:24:01 +0200

    change style

--- src/components/TransactionTable/TransactionTable.jsx
+++ src/components/TransactionTable/TransactionTable.jsx
@@ -13,24 +13,12 @@ export const TransactionTable = () => {
         </thead>
         <tbody>
           <tr>
-            <td className={css.cell} data-label="Date">
-              04.01.19
-            </td>
-            <td className={css.cell} data-label="Type">
-              +
-            </td>
-            <td className={css.cell} data-label="Category">
-              Regular Income
-            </td>
-            <td className={css.cell} data-label="Comment">
-              Sallary
-            </td>
-            <td className={css.cell} data-label="Sum">
-              10000
-            </td>
-            <td className={css.cell} data-label="Balance">
-              10000
-            </td>
+            <td data-label="Date">04.01.19</td>
+            <td data-label="Type">+</td>
+            <td data-label="Category">Regular Income</td>
+            <td data-label="Comment">Sallary</td>
+            <td data-label="Sum">10000</td>
+            <td data-label="Balance">10000</td>
           </tr>
           <tr>
             <td data-label="Date">08.01.19</td>
@@ -47,6 +35,14 @@ export const TransactionTable = () => {
             <td data-label="Comment">Gift for your wife</td>
             <td data-label="Sum">300</td>
             <td data-label="Balance">9400</td>
+          </tr>
+          <tr>
+            <td data-label="Date">04.01.19</td>
+            <td data-label="Type">-</td>
+            <td data-label="Category">Other</td>
+            <td data-label="Comment">Gift for your wife</td>
+            <td data-label="Sum">300</td>
+            <td data-label="Balance">9100</td>
           </tr>
           <tr>
             <td data-label="Date">04.01.19</td>

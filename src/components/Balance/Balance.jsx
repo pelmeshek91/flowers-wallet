8575e73ec68754b9fc5b713c470ac10a035d1e85
Lyuba Pushkina <lyuba.pushkina@gmail.com>
2022-11-14 20:00:34 +0000

    some changes

--- src/components/Balance/Balance.jsx
+++ src/components/Balance/Balance.jsx
@@ -1,13 +1,15 @@
-import css from "./Balance.module.css";
-import financeSelectors from "../../redux/transactions/transactionsSelector"
+import css from './Balance.module.css';
+import financeSelectors from '../../redux/transactions/transactionsSelector';
+import { useSelector } from 'react-redux';
 
 const Balance = () => {
-    return (
-        <div className={css.balance}>
-            <p className={css.balance__title}>Your Balance</p>
-            <p className={css.balance__total}>₴ {financeSelectors.selectTotalBalance}</p>
-        </div>
-    )
-}
+  const balance = useSelector(financeSelectors.selectTotalBalance);
+  return (
+    <div className={css.balance}>
+      <p className={css.balance__title}>Your Balance</p>
+      <p className={css.balance__total}>₴ {balance}</p>
+    </div>
+  );
+};
 
 export default Balance;
